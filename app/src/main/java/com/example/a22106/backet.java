93eb6487a0aaa2a7c612ace0e827fee721c1fa3f
mobil <20.08.2005darkness@gmail.com>
2023-05-19 11:57:40 +0700

    FirstCommit

---
+++ app/src/main/java/com/example/a22106/backet.java
@@ -0,0 +1,14 @@
+package com.example.a22106;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class backet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_backet);
+    }
+}
