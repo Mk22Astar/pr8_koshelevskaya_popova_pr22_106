dd78875340bad5d81c333a624c2f23b010d427b5
marina <londanv@yandex.ru>
2023-05-26 12:03:20 +0700

    start

--- app/src/main/java/com/example/a22106/MainActivity.java
+++ app/src/main/java/com/example/a22106/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.a22106;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button){
+            startActivity(new Intent(MainActivity.this, Product.class));
+        }
+
     }
 }
--- app/src/main/java/com/example/a22106/Product.java
+++ app/src/main/java/com/example/a22106/Product.java
@@ -2,13 +2,33 @@ package com.example.a22106;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Product extends AppCompatActivity {
-
+public class Product extends AppCompatActivity implements View.OnClickListener {
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        button = findViewById(R.id.button5);
+        button.setOnClickListener(this);
+        button = findViewById(R.id.button4);
+        button.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button5){
+            startActivity(new Intent(this, backet.class));
+        }
+        if(v.getId()==R.id.button4){
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo: 55.56754523, 88.45645343")));
+        }
+
+    }
+
 }
